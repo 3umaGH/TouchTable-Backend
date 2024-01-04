80f99462072197737f4d2c6d9fdefa3d7045a336
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-01-04 21:30:57 +0200

    refactor: decided not to use table status

--- src/mockData.ts
+++ src/mockData.ts
@@ -3,13 +3,11 @@ import { Category, Dish, Table } from "./types/globalTypes";
 export const mockTables: Table[] = [
   {
     id: 0,
-    status: "EMPTY",
     activeOrders: [],
   },
 
   {
     id: 1,
-    status: "EMPTY",
     activeOrders: [],
   },
 ];
